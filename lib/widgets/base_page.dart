aa61d215d947151b701da2da9adf37c7d2d026b8
Madson-Carvalho <madsonraynnon@gmail.com>
2024-10-26 18:18:12 -0300

    Finalização da página de home.

--- lib/widgets/base_page.dart
+++ lib/widgets/base_page.dart
@@ -25,7 +25,47 @@ class _BasePageState extends State<BasePage> {
         centerTitle: true,
       ),
       drawer: Drawer(
-        child: ListView(children: [DrawerHeader(child: child)],),
+        child: ListView(padding: EdgeInsets.zero, children: const [
+          UserAccountsDrawerHeader(
+            currentAccountPicture: CircleAvatar(),
+            accountEmail: Text('khalifa@example.com', style: TextStyle(color: Colors.black)),
+            accountName: Text(
+              'Khalifa do Brega',
+              style: TextStyle(fontSize: 24.0, color: Colors.black),
+            ),
+            decoration: BoxDecoration(
+                color: Color(0xFF00D09E),
+                ),
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text(
+              'Home',
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.add),
+            title: Text(
+              'Cadastrar despesas',
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.money_off),
+            title: Text(
+              'Despesas',
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.output),
+            title: Text(
+              'Sair',
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+        ]),
       ),
       body: widget.body,
     );
